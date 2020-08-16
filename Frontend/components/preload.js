872afb3fb6485da4566b5f77602c6012a44d8364
superdupermadboy <markusszar@gmail.com>
2020-08-16 20:11:21 +0200

    add build and first raw layout

--- Frontend/components/preload.js
+++ Frontend/components/preload.js
@@ -4,14 +4,30 @@ const { ipcRenderer } = require('electron');
 const { getAllSerialPorts } = require('../src/serial');
 
 window.addEventListener('DOMContentLoaded', () => {
+    let comportList = document.getElementById('comport');
+
+    getAllSerialPorts().then(allComports => {
+
+        allComports.forEach(comport => {
+
+            const listEntry = document.createElement('option');
+            listEntry.appendChild(document.createTextNode(comport.path));
+            listEntry.value = comport.path;
+
+            comportList.appendChild(listEntry);
+        })
+    
+    })
     const saveButton = document.getElementById('button-save');
 
     saveButton.addEventListener('click', () => {
-        const inputValue = document.getElementById('input-value').value;
+        const mode = document.getElementById('mode');
+        comportList = document.getElementById('comport');
 
-        ipcRenderer.send('update-lamp', inputValue);
+        ipcRenderer.send('update-lamp', {
+            comport: comportList.options[comportList.selectedIndex].value,
+            mode: mode.options[mode.selectedIndex].value,
+        });
     })
-
-    getAllSerialPorts();
   })
   
--- Frontend/src/serial.js
+++ Frontend/src/serial.js
@@ -7,29 +7,27 @@ let port;
 const sendToSerialPort = (data) => {
     console.log('sending this to serial port', data);
     
-    if (port && !port.isOpen()) {
-        port = new SerialPort('COM4', {
-            baudRate: 9600,
-            autoOpen: false,
-        });    
-    }
-
+    port = new SerialPort(data.comport, {
+        baudRate: 9600,
+        autoOpen: false,
+    });     
 
     port.open((err) => {
         console.log('Error happened while opening port: ', err);
     });
 
-    port.write(data);
-
     port.on('open', () => {
+        console.log('sending data');
+        port.write(data.mode);
         port.close();
     });
 }
 
-const getAllSerialPorts = () => {
-    SerialPort.list().then(data => {
-        console.log(data);
-    })
+const getAllSerialPorts = async () => {
+
+    const comPorts = await SerialPort.list();
+    
+    return comPorts;
 }
 
 module.exports = {
